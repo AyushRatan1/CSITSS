615c4498c36770812862251ef8875f03f3fb3178
AyushRatan1 <135870585+AyushRatan1@users.noreply.github.com>
2023-09-24 10:42:48 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
- window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () {
             let scrollPosition = window.pageYOffset;
             let backgroundPosition = `center ${50 - scrollPosition / 10}%`;
             document.getElementById("abc").style.backgroundPosition = backgroundPosition;
@@ -8,7 +8,7 @@
             return rect.top >= 0 && rect.left >= 0 && rect.bottom <= window.innerHeight && rect.right <= window.innerWidth;
         }
 
-       
+        // Handle the pop-up effect when the right-section is in the viewport
         function handleImagePopUp() {
             var popUpImage = document.getElementById('pop-up-image');
             if (isInViewport(popUpImage)) {
@@ -16,7 +16,7 @@
             }
         }
 
-        
+        // Add event listener for scrolling to handle the pop-up effect
         window.addEventListener('scroll', handleImagePopUp);
         function handleImageAppearance() {
             var parallax3 = document.querySelector('.parallax-3');
@@ -24,14 +24,14 @@
             if (parallax3) {
                 var rect = parallax3.getBoundingClientRect();
                 if (rect.bottom <= 0) {
-                    parallax3.classList.add('hide-image'); 
+                    parallax3.classList.add('hide-image'); // Hide the image by adding the hide-image class
                 } else {
-                    parallax3.classList.remove('hide-image'); 
+                    parallax3.classList.remove('hide-image'); // Show the image by removing the hide-image class
                 }
             }
         }
 
-        
+        // Add event listener for scrolling to handle the image appearance effect
         window.addEventListener('scroll', handleImageAppearance);
         
         function openPopUp(imageSrc) {
@@ -50,9 +50,9 @@
 
     imgPopups.forEach((img) => {
         img.addEventListener('mouseover', function () {
-            
+            // Update the source of the enlarged image
             popUpImage.src = img.src;
-            
+            // Show the pop-up overlay
             popUpOverlay.classList.add('active');
         });
 
@@ -61,3 +61,6 @@
             popUpOverlay.classList.remove('active');
         });
     });
+
+      // To show the notice box with animation
+document.querySelector('.notice-box').classList.add('active');
